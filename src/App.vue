3f49ba1d117299c487bc5501517280884c255b12
0101dd <aa09335665@gmail.com>
2022-01-10 16:15:41 +0800

    front

--- src/App.vue
+++ src/App.vue
@@ -8,21 +8,20 @@
         b-navbar-nav.ml-auto
           b-nav-item(v-if="!user.isLogin" to='/register') 註冊
           b-nav-item(v-if="!user.isLogin" to='/login') 登入
+          b-nav-item(v-if="user.isLogin && user.isAdmin" to='/admin') 管理
           b-nav-item(v-if="user.isLogin" @click="logout") 登出
   router-view
 </template>
 
 <script>
 export default {
-  computed: {
-    user () {
-      return this.$store.getters['user/user']
-    }
-  },
   methods: {
     logout () {
       this.$store.dispatch('user/logout')
     }
+  },
+  async created () {
+    this.$store.dispatch('user/getInfo')
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 Vue.use(VueRouter)
 
@@ -28,11 +29,59 @@ const routes = [
     meta: {
       title: '登入 | 購物網'
     }
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: () => import(/* webpackChunkName: "admin" */ '../views/Admin.vue'),
+    children: [
+      {
+        path: '',
+        name: 'AdminHome',
+        component: () => import(/* webpackChunkName: "admin" */ '../views/AdminHome.vue'),
+        meta: {
+          login: true,
+          admin: true,
+          title: '管理 | 購物網'
+        }
+      },
+      {
+        path: 'products',
+        name: 'AdminProducts',
+        component: () => import(/* webpackChunkName: "admin" */ '../views/AdminProducts.vue'),
+        meta: {
+          login: true,
+          admin: true,
+          title: '商品管理 | 購物網'
+        }
+      },
+      {
+        path: 'orders',
+        name: 'AdminOrders',
+        component: () => import(/* webpackChunkName: "admin" */ '../views/AdminOrders.vue'),
+        meta: {
+          login: true,
+          admin: true,
+          title: '訂單管理 | 購物網'
+        }
+      }
+    ]
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const user = store.getters['user/user']
+  if (to.meta.login && !user.isLogin) {
+    next('/login')
+  } else if (to.meta.admin && !user.isAdmin) {
+    next('/')
+  } else {
+    next()
+  }
 })
 
 router.afterEach((to, from) => {
